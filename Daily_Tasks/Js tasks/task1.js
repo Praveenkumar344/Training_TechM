0025c816914dfb84d8f1cc3453f7cbffed2c81dd
Praveenkumar T <90972479+Praveenkumar344@users.noreply.github.com>
2025-02-23 13:13:28 +0530

    Add files via upload

---
+++ Daily_Tasks/Js tasks/task1.js
@@ -0,0 +1,12 @@
+// 1. Without paasing parameters, display the multiplication 
+// table with two variables , one variable for the
+//  number , and other for the limit.
+var x=5;
+var times=10;
+function tables(){
+    for(let i=1;i<=times;i++){
+        console.log(x+" * "+i+" = "+(x*i));
+
+    }
+}
+tables();
