e7b9513e143e1ce148f847ddce15cda780bfc163
Praveenkumar T <90972479+Praveenkumar344@users.noreply.github.com>
2025-02-23 21:02:30 +0530

    Delete Task 3 - Java Advanced Programs directory

--- Task 3 - Java Advanced Programs/Task 4/ThrowExceptionExample.java
+++
@@ -1,9 +0,0 @@
-public class ThrowExceptionExample {
-    public static void main(String[] args) {
-        try {
-            int result = 10 / 0;
-        } catch (ArithmeticException e) {
-            System.out.println("Caught an exception: " + e.getMessage());
-        }
-    }
-}
